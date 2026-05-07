2b42093393e5c1fd0be48ea85503242d01ceb0da
jupo-ai <jupojupo.ai@gmail.com>
2026-05-07 12:06:35 +0900

    ImageLoaderで公式ノードの処理を呼び出し

--- py/nodes/image/image_loader.py
+++ py/nodes/image/image_loader.py
@@ -3,12 +3,9 @@ from ...utils import mk_name
 from .common import PACKAGE_NAME, CATEGORY
 
 import folder_paths
-import node_helpers
 from pathlib import Path
-import torch
-import numpy as np
-from PIL import Image, ImageOps, ImageSequence
 import hashlib
+from nodes import LoadImage
 
 
 # ===============================================
@@ -37,50 +34,8 @@ class ImageLoader(io.ComfyNode):
     
     @classmethod
     def execute(cls, image):
-        image_path = folder_paths.get_annotated_filepath(image)
-        img = node_helpers.pillow(Image.open, image_path)
-
-        output_images = []
-        output_masks = []
-        w, h = None, None
-        
-        execluded_formats = ["MPO"]
-
-        for i in ImageSequence.Iterator(img):
-            i = node_helpers.pillow(ImageOps.exif_transpose, i)
-
-            if i.mode == "I":
-                i = i.point(lambda i: i * (1 / 255))
-            image = i.convert("RGB")
-
-            if len(output_images) == 0:
-                w = image.size[0]
-                h = image.size[1]
-            
-            if image.size[0] != w or image.size[1] != h:
-                continue
-            
-            image = np.array(image).astype(np.float32) / 255.0
-            image = torch.from_numpy(image)[None, ]
-            if "A" in i.getbands():
-                mask = np.array(i.getchannel("A")).astype(np.float32) / 255.0
-                mask = 1. - torch.from_numpy(mask)
-            elif i.mode == "P" and "transparency" in i.info:
-                mask = np.array(i.convert("RGBA").getchannel("A")).astype(np.float32) / 255.0
-                mask = 1. - torch.from_numpy(mask)
-            else:
-                mask = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
-            output_images.append(image)
-            output_masks.append(mask)
-        
-        if len(output_images) > 1 and img.format not in execluded_formats:
-            output_image = torch.cat(output_images, dim=0)
-            output_mask = torch.cat(output_masks, dim=0)
-        else:
-            output_image = output_images[0]
-            output_mask = output_masks[0]
-        
-        return io.NodeOutput(output_image, output_mask)
+        output_images, output_masks = LoadImage().load_image(image)
+        return io.NodeOutput(output_images, output_masks)
     
     
     @classmethod
